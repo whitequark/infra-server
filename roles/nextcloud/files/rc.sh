273679195e66b85df2395d123565716fd6a1611e
whitequark <whitequark@whitequark.org>
2017-12-14 06:40:08 +0000

    owncloud → nextcloud.

--- roles/nextcloud/files/rc.sh
+++ roles/nextcloud/files/rc.sh
@@ -4,20 +4,20 @@ if [ true != "$INIT_D_SCRIPT_SOURCED" ] ; then
     set "$0" "$@"; INIT_D_SCRIPT_SOURCED=true . /lib/init/init-d-script
 fi
 ### BEGIN INIT INFO
-# Provides:          owncloud
+# Provides:          nextcloud
 # Required-Start:    $remote_fs $syslog
 # Required-Stop:     $remote_fs $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: spawn-fcgi wrapper for php-cgi running as owncloud
+# Short-Description: spawn-fcgi wrapper for php-cgi running as nextcloud
 ### END INIT INFO
 
 # Author: whitequark <whitequark@whitequark.org>
 
-DESC="owncloud"
-PIDFILE=/var/run/owncloud.pid
+DESC="nextcloud"
+PIDFILE=/var/run/nextcloud.pid
 DAEMON=/usr/bin/spawn-fcgi
-DAEMON_ARGS="-s /run/owncloud.sock -u owncloud -U www-data -P ${PIDFILE}"
+DAEMON_ARGS="-s /run/nextcloud.sock -u nextcloud -U www-data -P ${PIDFILE}"
 DAEMON_ARGS="${DAEMON_ARGS} -C 3 -- /usr/bin/php-cgi7.0"
 
 do_stop_cmd() {
